cd99ed33a2ade46e1137608d243d6dd8632bdd09
1up <124456837+1upfunniguy@users.noreply.github.com>
2025-01-16 17:04:12 -0700

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,4 @@
-function changeText(newText) {
-    const spanElement = document.querySelector('.dropdown > span');
-    spanElement.innerHTML = newText;
-}
-
-document.querySelector('.dropdown > span').addEventListener('mouseover', () => {
-    changeText('&#8681;Links&#8681;');
+document.querySelector('.dropdown span').addEventListener('click', () => {
+    const menu = document.querySelector('.dropdown-menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
-
-document.querySelector('.dropdown > span').addEventListener('mouseout', () => {
-    changeText('&#8679;Links&#8679;');
-});
-
