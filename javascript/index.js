c4cd740a200cd5e0719ea376f29e7bc93ff86655
1up <124456837+1upfunniguy@users.noreply.github.com>
2025-01-16 17:17:00 -0700

    Delete javascript directory

--- javascript/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.dropdown span').addEventListener('click', () => {
-    const menu = document.querySelector('.dropdown-menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
